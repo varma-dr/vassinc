8fee1560fac6b57fcd33ebb9da06803951c62606
vamra-dr <varma.dr16@gmail.com>
2025-03-12 14:33:00 -0400

    test work - 1

--- frontend/src/pages/Candidate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Candidate = () => {
-  return (
-    <div>Candidate</div>
-  )
-}
-
-export default Candidate
--- frontend/src/pages/Recruiter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Recruiter = () => {
-  return (
-    <div>Recruiter</div>
-  )
-}
-
-export default Recruiter
