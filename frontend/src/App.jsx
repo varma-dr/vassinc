a3812cc8dc6389e4bbb50fce5e7d8282876983c2
vamra-dr <varma.dr16@gmail.com>
2025-03-26 13:13:00 -0400

    updated tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
+
+
 function App() {
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="rounded-lg bg-white p-8 shadow-lg">
-        <h1 className="text-3xl font-bold text-green-700">Welcome To VASS INC.</h1>
-        <p className="mt-2 text-gray-600">Tailwind CSS is working!</p>
-      </div>
-    </div>
+    <>
+      <h1 className="text-red-400 text-3xl">Vass Inc</h1>
+    </>
   )
 }
 
