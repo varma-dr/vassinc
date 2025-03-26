cab0cb9641de39720a7e849a3d5c38a530ac01bf
vamra-dr <varma.dr16@gmail.com>
2025-03-26 13:15:27 -0400

    inline style working but tailwind is not working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
-
-
 function App() {
-
   return (
     <>
-      <h1 className="text-red-400 text-3xl">Vass Inc</h1>
+      <h1 className="text-red-400 text-3xl" style={{color: 'red'}}>Vass Inc</h1>
     </>
   )
 }
-
 export default App
