a542584f15469139a49196a191fd746aa52f886f
vamra-dr <varma.dr16@gmail.com>
2025-03-31 23:22:23 -0400

    Changed import statement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage/>} />
       </Routes>
     </Router>
   );
