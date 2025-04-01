a5a6d30d19b2c7cdf48762681cc28a79c0abce74
vamra-dr <varma.dr16@gmail.com>
2025-03-31 23:21:12 -0400

    Changed import statements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/LoginPage";
 
 function App() {
   return (
