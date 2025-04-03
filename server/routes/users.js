71ca480b3f019bf914f4de0283700bdbfca2bc01
vamra-dr <varma.dr16@gmail.com>
2025-04-03 01:22:52 -0400

    User routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/userController');
 
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
 router.post('/', registerUser);
 
 module.exports = router;
