262a6d102eb34e968f20798575d53b00918c178e
vamra-dr <varma.dr16@gmail.com>
2025-04-02 23:14:35 -0400

    Updated using ES modules instead of CommonJS

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -25,4 +25,6 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+export default User;
