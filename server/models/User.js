5cb82a27bb91d5a6e0fef6bdaad97571651c6e6a
vamra-dr <varma.dr16@gmail.com>
2025-04-02 23:10:07 -0400

    updated Userdata

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userType: {
+    type: String,
+    enum: ['candidate', 'recruiter', 'employee', 'admin'],
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
