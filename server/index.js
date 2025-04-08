d8f35245e3ef5e7b42740e16b7cb30111870eb6c
vamra-dr <varma.dr16@gmail.com>
2025-04-08 15:57:54 -0400

    imported cors package

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./db'); 
+const cors = require('cors'); 
 require('dotenv').config();
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(cors());
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth')); 
+app.use('/api/auth', require('./routes/auth'));
 
 app.get('/', (req, res) => res.send('API Running'));
 
