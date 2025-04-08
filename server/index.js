545c0843c6f7a2bf0aaf58b4fcc97a0d46419e93
vamra-dr <varma.dr16@gmail.com>
2025-04-08 15:55:42 -0400

    Updated path to db.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const connectDB = require('./db'); 
 require('dotenv').config();
 
 const app = express();
@@ -12,7 +11,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-// Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth')); 
 
