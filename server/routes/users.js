60e149be2d52dae2b5fda9643f5198e29c53261e
vamra-dr <varma.dr16@gmail.com>
2025-04-08 15:32:49 -0400

    Updated to connect with DB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/userController');
+const { registerUser, updateUserType } = require('../controllers/userController');
+const auth = require('../middleware/auth');
+
 
 router.post('/', registerUser);
+
+router.post('/update-type', auth, updateUserType);
 
 module.exports = router;
