075120f50d1963862ec8637482e1047f5593f860
vamra-dr <varma.dr16@gmail.com>
2025-04-10 14:48:39 -0400

    fixed error in user.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, updateUserType } = require('../controllers/userController');
+
+// Import the controller functions
+const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
+// Make sure the functions exist before using them
+const registerUser = userController.registerUser || 
+  ((req, res) => res.status(500).json({ msg: 'Registration function not implemented' }));
+
+const updateUserType = userController.updateUserType || 
+  ((req, res) => res.status(500).json({ msg: 'Update user type function not implemented' }));
+
+// Define routes with fallback for auth middleware
 router.post('/', registerUser);
-router.post('/update-type', auth, updateUserType);
+router.post('/update-type', 
+  typeof auth === 'function' ? auth : (req, res, next) => next(), 
+  updateUserType
+);
 
 module.exports = router;
